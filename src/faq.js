a0fb0566a93c2395d187961c4b6256593de83508
sai <saivivek1414@gmail.com>
2023-07-23 20:27:16 +0530

    \/

--- src/faq.js
+++ src/faq.js
@@ -26,13 +26,13 @@ const Faq=()=>{
                 <i class="fa-solid fa-angle-down"></i>
                 </div>
                 <div className={`${activeanswer("q1")}`} onClick={()=>activeq("q1")}>
-                    {active===active &&<p>Comparing rental car deals is important as it helps find the
+                 <p>Comparing rental car deals is important as it helps find the
                   best deal that fits your budget and requirements, ensuring you
                   get the most value for your money. By comparing various
                   options, you can find deals that offer lower prices,
                   additional services, or better car models. You can find car
                   rental deals by researching online and comparing prices from
-                  different rental companies.</p>}
+                  different rental companies.</p>
                   </div>
             </div>
             <div className="accordion1">
@@ -40,27 +40,31 @@ const Faq=()=>{
                 <h4 style={{fontWeight:'500'}}>2. How do I find the car rental deals?</h4>
                 <i class="fa-solid fa-angle-down"></i>
                 </div>
-                {active==="q2"&& <p style={{color:'#706f7b',padding:'15px 40px 30px 40px',lineHeight:'1.7',fontFamily:'rubik'}} onClick={()=>activeq("q2")}>You can find car rental deals by researching online and
+                <div className={`${activeanswer("q2")}`} onClick={()=>activeq("q1")}>
+                <p>You can find car rental deals by researching online and
                   comparing prices from different rental companies. Websites
                   such as Expedia, Kayak, and Travelocity allow you to compare
                   prices and view available rental options. It is also
                   recommended to sign up for email newsletters and follow rental
                   car companies on social media to be informed of any special
-                  deals or promotions.</p>}
+                  deals or promotions.</p>
+                  </div>
             </div>
             <div className="accordion1">
             <div className={`${activequestion("q3")}`} onClick={()=>activeq("q3")}>
                 <h4 style={{fontWeight:'500'}}>3. How do I find such low rental car prices?</h4>
                 <i class="fa-solid fa-angle-down"></i>
                 </div>
-                {active==="q3"&& <p style={{color:'#706f7b',padding:'15px 40px 30px 40px',lineHeight:'1.7',fontFamily:'rubik'}} onClick={()=>activeq("q3")}>Book in advance: Booking your rental car ahead of time can
+                <div className={`${activeanswer("q3")}`} onClick={()=>activeq("q1")}>
+                 <p>Book in advance: Booking your rental car ahead of time can
                   often result in lower prices. Compare prices from multiple
                   companies: Use websites like Kayak, Expedia, or Travelocity to
                   compare prices from multiple rental car companies. Look for
                   discount codes and coupons: Search for discount codes and
                   coupons that you can use to lower the rental price. Renting
                   from an off-airport location can sometimes result in lower
-                  prices.</p>}
+                  prices.</p>
+                  </div>
             </div>
             </div>
             </div>
